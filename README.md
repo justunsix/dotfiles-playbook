e49b9ca1846a0155cc6ab2f214da7f8b51d3b82d
Justin Tung <justin.tung@live.ca>
2023-03-25 12:35:22 -0400

    base Ansible playbooks, requirements, README and start script

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# dotfiles-playbook
+# Dotfiles-Playbook
+
 Ansible playbooks for Managing my Linux (Ubuntu, Fedora) and Windows Machines
+
+## Usage
+
+Run `bootstrap.sh`.
+
+Script will check the Linux / WSL distribution and run the appropriate Ansible playbook and install required software and Ansible requirements for the target system.
+
+## TODO
+
+- Document overlay using `config.yml` and overlay directory
+- Make vars and tasks more flexible instead of hardcoding value with:
+  - Use [`ansible.builtin.setup`](https://docs.ansible.com/ansible/latest/collections/ansible/builtin/setup_module.html) settings
+    - `Ansible_distribution` for conditionals on Fedora and Ubuntu playbook
+  - Build on `default_config.yaml` and override for Fedora
+- Set up [Ansible logging](https://docs.ansible.com/automation-controller/latest/html/administration/logging.html) to more easily read output outside the terminal
---
+++ bootstrap.sh
@@ -0,0 +1,221 @@
+#!/usr/bin/env bash
+
+# Script to run on a new Linux system to configure it or
+# run on a new WSL Windows system to configure Windows:
+# 1. First, check if the Linux distribution is Ubuntu, Fedora, or WSL Ubuntu. If it is not one of those, the script exits.
+# 2. Installs any prequisites needed for the Ansible playbook and software more easily installed in command line
+# 3. Checks if an SSH RSA key exists. If it doesn't, the create one
+# 4. If the bootstrap script has an argument "-r" and there are Ansible requirements, install them
+# 5. Run the Ansible playbook.
+
+set -e
+
+##########################################
+## Variables
+##########################################
+
+DOTFILES="$HOME/Code/dotfiles-playbook"
+DOTSSH="$HOME/.ssh"
+
+# System Flag
+isUbuntu="false"
+isFedora="false"
+isWSLUbuntu="false"
+
+# Restart shell flag
+restartShell="false"
+
+# If Linux distribution is Ubuntu, set isUbuntu variable to "true"
+if [ -f /etc/os-release ]; then
+	# Get os-release variables
+	. /etc/os-release
+	if [ "$ID" = "ubuntu" ]; then
+		isUbuntu="true"
+	fi
+fi
+
+# If file exist at /mnt/c, it is WSL
+if [ -f /mnt/c/Windows/System32/wsl.exe ]; then
+	isWSLUbuntu="true"
+	isUbuntu="false"
+fi
+
+# If Linux distribution is Fedora, set isFedora variable to "true"
+if [ -f /etc/fedora-release ]; then
+	isFedora="true"
+fi
+
+# If Linux distribution is not Ubuntu or Fedora, exit
+if [ "$isUbuntu" = "false" ] && [ "$isFedora" = "false" ] && [ "$isWSLUbuntu" = "false" ]; then
+	echo "This Linux distribution is not supported by this script."
+	exit 1
+fi
+
+##########################################
+## Functions
+##########################################
+
+install_ansible() {
+
+	# Check if Ansible if installed
+	# If Ansible is not installed, install it, assumes python3 is installed already
+	# per https://docs.ansible.com/ansible/latest/installation_guide/intro_installation.html
+	# as of 2023-03-25
+	if ! [ -x "$(command -v ansible)" ]; then
+		echo "Ansible is not installed. Installing Ansible using pip..."
+
+		if [ "$isUbuntu" = "true" ]; then
+			sudo apt install python3-pip -y
+		fi
+
+		if [ "$isFedora" = "true" ]; then
+			sudo dnf install python3-pip -y
+		fi
+
+		if [ "$isWSLUbuntu" = "true" ]; then
+			sudo apt install python3-pip -y
+			python3 -m pip install --user pywinrm
+		fi
+
+		python3 -m pip install --user ansible
+
+		restartShell="true"
+
+	fi
+
+}
+
+# Installs commands that the Ansible playbook needs
+# or software that is easier to install in command line
+install_prequisites() {
+
+  install_ansible
+
+	# Check if Starship is installed
+	if ! [ -x "$(command -v starship)" ]; then
+		if [ "$isFedora" = "true" ]; then
+			# Install starship
+			# per https://starship.rs/guide/#%F0%9F%9A%80-installation
+			# as of 2023-03-25
+			echo "Installing Starship..."
+			curl -sS https://starship.rs/install.sh | sh
+		fi
+	fi
+
+	# Check if deb-get is installed for Ubuntu native
+	if [[ "$isUbuntu" = "true" && "$isWSLUbuntu" = "false" ]]; then
+		if ! [ -x "$(command -v deb-get)" ]; then
+			# Install deb-get
+			# per https://github.com/wimpysworld/deb-get
+			# as of 2023-03-25
+			echo "deb-get is not installed. Installing deb-get..."
+			sudo apt install curl
+			curl -sL https://raw.githubusercontent.com/wimpysworld/deb-get/main/deb-get | sudo -E bash -s install deb-get
+		fi
+	fi
+
+	if [[ "$isWSLUbuntu" = "false" ]]; then
+
+		# Check nix is installed
+		if ! [ -x "$(command -v nix-env)" ]; then
+			echo "Installing Nix..."
+			# Install Nix single user installation
+			# per https://nixos.wiki/wiki/Nix_Installation_Guide
+			# as of 2023-03-25
+			sudo install -d -m755 -o "$(id -u)" -g "$(id -g)" /nix
+			curl -L https://nixos.org/nix/install | sh
+
+			# Add Ansible and Nix bin to PATH
+			echo "export PATH=$PATH:$HOME/.nix-profile/bin/:$HOME/.local/bin" >>~/.bashrc
+			echo "source $HOME/.nix-profile/etc/profile.d/nix.sh" >>~/.bashrc
+
+			restartShell="true"
+		fi
+
+	fi
+
+	# Check rustup is installed
+	if ! [ -x "$(command -v rustup)" ]; then
+		echo "Installing Rustup..."
+		# Install Rustup
+		# per https://www.rust-lang.org/tools/install
+		# as of 2023-02-20
+		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+		restartShell="true"
+	fi
+
+	# if restartShell is true, tell user to restart shell and exit script
+	if [ "$restartShell" = "true" ]; then
+		# Since sourcing does not work for nix and rust requires shell restart to get PATH changes
+		echo "Restart shell to get PATH changes"
+		echo "and re-run with bootstrap.sh -r"
+		exit 0
+	fi
+
+}
+
+# Install Ansible requirements in directory
+install_ansible_requirements() {
+	if [ -f requirements.yml ]; then
+		echo "Ansible requirements exist. Installing Ansible requirements..."
+		if [ "$isWSLUbuntu" = "true" ]; then
+			ansible-galaxy install -r requirements-windows.yml
+			# else install requirements.yml
+		else
+			ansible-galaxy install -r requirements.yml
+		fi
+	fi
+}
+
+setup_RSA_key() {
+
+	# Check if SSH RSA key exists
+	# If SSH RSA key does not exist, create it
+	# Ensure keys are are only readable by user
+	if [ ! -f "$DOTSSH/id_rsa" ]; then
+		echo "SSH RSA key does not exist. Creating SSH RSA key..."
+		mkdir -p "$DOTSSH"
+		chmod 700 "$DOTSSH"
+		ssh-keygen -b 4096 -t rsa -f "$DOTSSH/id_rsa" -N "" -C "$USER@$HOSTNAME"
+		cat "$DOTSSH/id_rsa.pub" >>"$DOTSSH/authorized_keys"
+		chmod 600 "$DOTSSH/authorized_keys"
+	fi
+
+}
+
+#################################
+## Main Start of Script
+#################################
+
+install_prequisites
+# Check if shell script has an argument "-r" (has requirements), if yes, check requirements
+if [ "$1" = "-r" ]; then
+	install_ansible_requirements
+fi
+
+setup_RSA_key
+
+cd "$DOTFILES"
+
+# ==== Run Ansible Playbook ====
+
+if [ -f "$DOTFILES/vault-password.txt" ]; then
+	ansible-playbook --diff --vault-password-file "$DOTFILES/vault-password.txt" "$DOTFILES/ubuntu.yml" -v
+else
+
+	if [ "$isUbuntu" = "true" ]; then
+		# per https://docs.ansible.com/ansible/latest/cli/ansible-playbook.html
+		# --diff : when changing files/templates, show the differences in files
+		# --ask-become-pass : ask for privilege escalation password
+		# -v : low verbose mode, can increase with -vvv...
+		ansible-playbook --diff "$DOTFILES/ubuntu.yml" --ask-become-pass -v
+	fi
+
+	if [ "$isFedora" = "true" ]; then
+		ansible-playbook --diff "$DOTFILES/fedora.yml" --ask-become-pass -v
+	fi
+
+	if [ "$isWSLUbuntu" = "true" ]; then
+		ansible-playbook --diff "$DOTFILES/windows.yml" --ask-pass -v
+	fi
+fi
