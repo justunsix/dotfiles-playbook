44ae81c7f860440efba41486caba45c1b43a44e2
Justin Tung <justin.tung@live.ca>
2025-01-01 12:08:53 -0500

    fix: possible issues after installing nix

--- scripts/install-nix.sh
+++ scripts/install-nix.sh
@@ -10,6 +10,11 @@ if ! command -v nix &>/dev/null; then
   # --yes is to automatically agree to the installation
   sh <(curl -L https://nixos.org/nix/install) --daemon --yes
 
+  # Source to enable nix for later commands
+  if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
+    . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
+  fi
+
 fi
 
 echo -e "-- nix Installed"
